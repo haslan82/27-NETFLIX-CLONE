dee8642ead1cc3f40e757ce3b8487ed31717d4b1
haslan82 <haslan82@hotmail.com>
2024-06-15 01:14:51 +0300

    sona doğru

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,51 @@
+import { useSelector } from "react-redux";
+import Loader from "./Loader";
+import Error from "./Error";
 
 
 const Hero = () => {
+  const { isLoading, error, movies } = useSelector((store) => store.movies);
+  console.log(movies);
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 md:max-h-[400px] gap-5 mb-10 '>
-     <div className='flex flex-col gap-6 justify-center items-center'>
-        <h1 className='text-3xl font-bold'>Tarot</h1>
-        <p className='text-start'>Bir grup arkadaş, Tarot fallarının kutsal kuralını pervasızca ihlal ettiğinde bilmeden, 
-            lanetli kartların içinde hapsolmuş, anlatılamaz bir kötülüğü serbest bırakırlar. 
-            Birer birer kaderle yüzleşirler ve ölümle yarışırlar.</p>
-            <p>
-                <span className=''>IMDB :</span>
-                <span className='text-yellow-400 ms-2 font-semibold'>6.5</span>
+    <div className="grid grid-cols-1 md:grid-cols-2 md:max-h-[400px] gap-5 mb-10 ">
+      {!movies || isLoading ? (
+        <Loader />
+      ) : error ? (
+        < Error />
+      ) : (
+        <>
+          <div className="flex flex-col gap-6 justify-center items-center">
+            <h1 className="text-3xl font-bold">Tarot</h1>
+            <p className="text-start">
+              Bir grup arkadaş, Tarot fallarının kutsal kuralını pervasızca
+              ihlal ettiğinde bilmeden, lanetli kartların içinde hapsolmuş,
+              anlatılamaz bir kötülüğü serbest bırakırlar. Birer birer kaderle
+              yüzleşirler ve ölümle yarışırlar.
             </p>
-            <div className='flex gap-5'>
-                <button className='p-2  bg-red-600 rounded hover:bg-red-700'>Film İzle</button>
-                <button className='p-2  bg-blue-600 rounded hover:bg-blue-700'>Listeye Ekle</button>
+            <p>
+              <span className="">IMDB :</span>
+              <span className="text-yellow-400 ms-2 font-semibold">6.5</span>
+            </p>
+            <div className="flex gap-5">
+              <button className="p-2  bg-red-600 rounded hover:bg-red-700">
+                Film İzle
+              </button>
+              <button className="p-2  bg-blue-600 rounded hover:bg-blue-700">
+                Listeye Ekle
+              </button>
             </div>
-     </div>
-     <div>
-        <img className=' hero-img my-4 object-contain rounded max-h-[300px] ' src="https://image.tmdb.org/t/p/original/oavbmL3iddJUmC8nQjL6bLHwAP4.jpg" alt="" />
-     </div>
+          </div>
+        </>
+      )}
+      <div>
+        <img
+          className=" hero-img my-4 object-contain rounded max-h-[300px] "
+          src="https://image.tmdb.org/t/p/original/oavbmL3iddJUmC8nQjL6bLHwAP4.jpg"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,50 +1,48 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "./../utils/api";
 import Loader from "../components/Loader";
 import { baseImgUrl } from "../constants";
 import DetailDisplay from "../components/DetailDisplay";
 import millify from "millify";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import ActorCard from "../components/ActorCard";
+
 
 const DetailPage = () => {
   const [movie, setMovie] = useState(null);
-
-  // url den filmin id sini useParams ile aldık
+  //* URL'den filmin idsini useParams methodu ile aldık.
   const { id } = useParams();
-  // console.log(id);
 
   useEffect(() => {
-
-const params ={
-  append_to_response:"credits, videos",
-};
-
+    const params = {
+      append_to_response: "credits,videos",
+    };
 
     api
-      .get(`movie/${id} `,{params})
+      .get(`movie/${id}`, { params })
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err));
   }, []);
-  console.log(movie);
-
+ // console.log(movie);
   return (
     <div>
       {!movie ? (
         <Loader />
       ) : (
         <div>
-          {/* // üst ALAN */}
-          <div className="relative  h-[30vh]">
+          {/* üst alan */}
+          <div className="relative  h-[20vh]">
             <img
               className="h-full w-full object-cover"
               src={baseImgUrl + movie.backdrop_path}
               alt=""
             />
-            <div className="absolute bg-black inset-0 bg-opacity-50 grid place-items-center ">
-              <h2 className="text-3xl font-semibold">{movie.title} </h2>
+            <div className="absolute bg-black inset-0 bg-opacity-50 grid place-items-center">
+              <h2 className="text-3xl font-semibold">{movie.title}</h2>
             </div>
           </div>
-
           {/* orta alan */}
           <div className="my-10 grid gird-cols-1 md:grid-cols-2">
             <div>
@@ -62,29 +60,40 @@ const params ={
                 data={movie.production_countries}
               />
             </div>
-          
-          <div className="flex flex-col gap-2">
-            <p>{movie.overview} </p>
-            <p>
-              <span>Bütçe : </span>
-              <span className="text-blue-500 ms-2">
-                ${millify(movie.budget)}{" "}
-              </span>
-            </p>
-            <p>
-              <span>Hasılat :</span>
-              <span className="text-green-500 ms-2">
-                ${millify(movie.revenue)}{" "}
-              </span>
-            </p>
-          </div>
+
+            <div className="flex flex-col gap-2">
+              <p>{movie.overview}</p>
+              <p>
+                <span>Bütçe:</span>
+                <span className="text-green-500 ms-2">
+                  ${millify(movie.budget)}
+                </span>
+              </p>
+              <p>
+                <span>Hasılat:</span>
+                <span className="text-green-500 ms-2">
+                  ${millify(movie.revenue)}
+                </span>
+              </p>
+            </div>
           </div>
 
           {/* slider alanı */}
-<div>
-
-</div>
-
+          <div>
+            <Splide
+              options={{
+                pagination: false,
+                autoWidth: true,
+                gap: "10px",
+              }}
+            >
+              {movie.credits.cast.map((actor, i) => (
+                <SplideSlide>
+                  <ActorCard key={i} actor={actor} />
+                </SplideSlide>
+              ))}
+            </Splide>
+          </div>
         </div>
       )}
     </div>
@@ -92,3 +101,4 @@ const params ={
 };
 
 export default DetailPage;
+
